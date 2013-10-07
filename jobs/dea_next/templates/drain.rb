229561efa6beab7c36f9b3ff2cb21dbed7a247de
Alex Suraci and Maria Shaldibina <pair+alex+mariash@pivotallabs.com>
2013-10-07 15:47:16 -0700

    wait for staging tasks to complete while draining

--- jobs/dea_next/templates/drain.rb
+++ jobs/dea_next/templates/drain.rb
@@ -2,6 +2,7 @@
 
 require "logger"
 require "fileutils"
+require "json"
 
 FileUtils.mkdir_p("/var/vcap/sys/log/dea_next")
 logger = Logger.new("/var/vcap/sys/log/dea_next/drain.log")
@@ -12,9 +13,7 @@ logger.info("Drain script invoked with #{ARGV.join(" ")}")
 
 dea_pidfile = "/var/vcap/sys/run/dea_next/dea_next.pid"
 warden_pidfile = "/var/vcap/sys/run/warden/warden.pid"
-
-# give the DEAS a while to evacuate and restart apps
-default_timeout = <%= p("dea_next.evacuation_delay") %>
+snapshot_path = "/var/vcap/data/dea_next/db/instances.json"
 
 if !File.exists?(dea_pidfile)
   logger.info("DEA not running")
@@ -32,16 +31,25 @@ begin
   dea_pid = File.read(dea_pidfile).to_i
   warden_pid = File.read(warden_pidfile).to_i
 
-  logger.info("Sending signal USR2 to Warden.")
-  Process.kill("USR2", warden_pid)
+  staging_tasks = File.exists?(snapshot_path) ? JSON.load(File.open(snapshot_path))["staging_tasks"] : []
 
-  logger.info("Sending signal KILL to DEA.")
-  Process.kill("KILL", dea_pid)
+  if staging_tasks.empty?
+    logger.info("Sending signal USR2 to Warden.")
+    Process.kill("USR2", warden_pid)
 
-  timeout = 0
+    logger.info("Sending signal KILL to DEA.")
+    Process.kill("KILL", dea_pid)
+
+    timeout = 0
+  else
+    logger.info("Sending signal USR1 to DEA to trigger shutdown message.")
+    Process.kill("USR1", dea_pid)
+
+    timeout = -10
+  end
+
   logger.info("Setting timeout as #{timeout}.")
   puts timeout
-
 rescue Errno::ESRCH => e
   logger.info("Caught exception: #{e}")
   puts 0
